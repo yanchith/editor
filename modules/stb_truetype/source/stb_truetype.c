3f8d1cb616be552d314cb853ba2f06df4ba76aa4
yanchith <yanchi.toth@gmail.com>
2026-01-07 11:25:54 +0100

    Clean up the generate.jai

--- modules/stb_truetype/source/stb_truetype.c
+++ modules/stb_truetype/source/stb_truetype.c
@@ -1,11 +1,11 @@
+// This c file exists so that we can make a standalone library out of the h file.
+
 #ifdef WIN32
 #define __EXPORT __declspec(dllexport)
 #else
 #define __EXPORT
 #endif
 
-// NOTE(jt): I am not sure what __EXPORT is, but without Bindings_Generator does not see the
-// functions. .c files for STB libraries shipping with the compiler analogous defines.
 #define STBTT_DEF extern __EXPORT
 
 #define STB_TRUETYPE_IMPLEMENTATION
